8c006c7fe62b0a7039716df1b525a5f805630137
Enzo Monsalves <ingeniero.monsalves@gmail.com>
2024-06-05 21:35:46 -0400

    Ajustes

--- app.js
+++ app.js
@@ -5,36 +5,59 @@ const BaileysProvider = require('@bot-whatsapp/provider/baileys')
 const MockAdapter = require('@bot-whatsapp/database/mock')
 
 const { chat } = require('./scripts/chatGpt.js')
+
+const calculateTimeUntilMidnight = () => {
+    const now = new Date();
+    const midnight = new Date();
+    midnight.setHours(24, 0, 0, 0);
+  
+    const timeDifference = midnight - now;
+  
+    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+  
+    return { days, hours, minutes, seconds };
+  };
+
+const flowPrincipal = addKeyword(["hola"]).addAnswer("Hola, soy un chat bot diseñado para ayudar a los usuarios a resolver sus dudas, estoy en entrenamiento, por favor, se paciente conmigo")
+const flowPrincipal2 = addKeyword(["amor"]).addAnswer("Hola, BB, ¿Cómo estás? ¿En qué puedo ayudarte?")
+const flowPrincipal3 = addKeyword(["enzo"]).addAnswer("recuerda quedan para la medianoche: " + calculateTimeUntilMidnight().hours + " horas, " + calculateTimeUntilMidnight().minutes + " minutos y " + calculateTimeUntilMidnight().seconds + " segundos")
+
+// #region OpenAI
 // const flowPrincipal = addKeyword(EVENTS.WELCOME)
-const flowPrincipal = addKeyword(["hola"] ).addAnswer("Hola, soy un chat bot diseñado para ayudar a los usuarios a resolver sus dudas, estoy en entrenamiento, por favor, se paciente conmigo")
-const flowPrincipal2 = addKeyword(["amor"] ).addAnswer("Hola, BB, ¿Cómo estás? ¿En qué puedo ayudarte?")
+// .addAction(async (ctx, ctxFn) => {
+//     const prompt = "Soy un chat bot diseñado para ayudar a los usuarios a resolver sus dudas";
+//     const text = ctx.body;
 
-    // .addAction(async (ctx, ctxFn) => {
-    //     const prompt = "Soy un chat bot diseñado para ayudar a los usuarios a resolver sus dudas";
-    //     const text = ctx.body;
+//     const conversations = [];
 
-    //     const conversations = [];
+//     // Crea el contexto con las operaciones 
+//     const contextMessages = conversations.flatMap((conv) => [
+//         { role: "user", content: conv.question },
+//         { role: "assistant", content: conv.answer }
+//     ]);
 
-    //     // Crea el contexto con las operaciones 
-    //     const contextMessages = conversations.flatMap((conv) => [
-    //         { role: "user", content: conv.question },
-    //         { role: "assistant", content: conv.answer }
-    //     ]);
+//     // Añadir la pregunta al contexto
+//     contextMessages.push({ role: "user", content: text });
 
-    //     // Añadir la pregunta al contexto
-    //     contextMessages.push({ role: "user", content: text });
+//     // Obtener la respuesta de OpenAI
+//     const response = await chat(prompt, contextMessages);
 
-    //     // Obtener la respuesta de OpenAI
-    //     const response = await chat(prompt, contextMessages);
-
-    //     // Enviar la respuesta al usuario
-    //     await ctxFn.flowDynamic(response);
-    // });
-
+//     // Enviar la respuesta al usuario
+//     await ctxFn.flowDynamic(response);
+// });
+// #endregion
 
 const main = async () => {
     const adapterDB = new MockAdapter()
-    const adapterFlow = createFlow([flowPrincipal, flowPrincipal2])
+    const adapterFlow = createFlow([
+        flowPrincipal,
+        flowPrincipal2,
+        flowPrincipal3
+    ])
+
     const adapterProvider = createProvider(BaileysProvider)
 
     createBot({
