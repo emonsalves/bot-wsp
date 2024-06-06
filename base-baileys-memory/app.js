16a05d1cb1e6f655d4ba1f4b76a9d06de5455f35
Enzo Monsalves <ingeniero.monsalves@gmail.com>
2024-06-05 21:12:16 -0400

    Base Bot

---
+++ base-baileys-memory/app.js
@@ -0,0 +1,49 @@
+const { createBot, createProvider, createFlow, addKeyword, EVENTS } = require('@bot-whatsapp/bot')
+
+const QRPortalWeb = require('@bot-whatsapp/portal')
+const BaileysProvider = require('@bot-whatsapp/provider/baileys')
+const MockAdapter = require('@bot-whatsapp/database/mock')
+
+const { chat } = require('./scripts/chatGpt.js')
+// const flowPrincipal = addKeyword(EVENTS.WELCOME)
+const flowPrincipal = addKeyword(["hola"] ).addAnswer("Hola, soy un chat bot diseñado para ayudar a los usuarios a resolver sus dudas, estoy en entrenamiento, por favor, se paciente conmigo")
+const flowPrincipal2 = addKeyword(["amor"] ).addAnswer("Hola, BB, ¿Cómo estás? ¿En qué puedo ayudarte?")
+
+    // .addAction(async (ctx, ctxFn) => {
+    //     const prompt = "Soy un chat bot diseñado para ayudar a los usuarios a resolver sus dudas";
+    //     const text = ctx.body;
+
+    //     const conversations = [];
+
+    //     // Crea el contexto con las operaciones 
+    //     const contextMessages = conversations.flatMap((conv) => [
+    //         { role: "user", content: conv.question },
+    //         { role: "assistant", content: conv.answer }
+    //     ]);
+
+    //     // Añadir la pregunta al contexto
+    //     contextMessages.push({ role: "user", content: text });
+
+    //     // Obtener la respuesta de OpenAI
+    //     const response = await chat(prompt, contextMessages);
+
+    //     // Enviar la respuesta al usuario
+    //     await ctxFn.flowDynamic(response);
+    // });
+
+
+const main = async () => {
+    const adapterDB = new MockAdapter()
+    const adapterFlow = createFlow([flowPrincipal, flowPrincipal2])
+    const adapterProvider = createProvider(BaileysProvider)
+
+    createBot({
+        flow: adapterFlow,
+        provider: adapterProvider,
+        database: adapterDB,
+    })
+
+    QRPortalWeb()
+}
+
+main()
---
+++ base-baileys-memory/scripts/chatGpt.js.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const OpenAI = require("openai");
+
+const openaiApiKey = process.env.OPENAI_API_KEY;
+
+const chat = async (prompt, messages) => {
+    try {
+        const openai = new OpenAI({
+            apiKey: openaiApiKey,
+        });
+        const completion = await openai.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            messages: [
+                {
+                    role: "system",
+                    content: prompt
+                },
+                ...messages
+            ],
+        });
+        const answ = completion.data.choices[0].message.content;
+        return answ;
+    }
+    catch (error) {
+        console.error("Error al conectar con OpenAI", error);
+        return "Error al conectar con OpenAI";
+    }
+}
+
+module.exports = { chat }
