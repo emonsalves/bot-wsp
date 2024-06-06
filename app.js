e7347167357c70350c067b0c12cec627403cd0a3
Enzo Monsalves <ingeniero.monsalves@gmail.com>
2024-06-06 00:10:09 -0400

    Ajuste

--- app.js
+++ app.js
@@ -6,41 +6,79 @@ const MockAdapter = require('@bot-whatsapp/database/mock')
 
 const { chat } = require('./scripts/chatGpt.js')
 
-const flowPrincipal = addKeyword(["hola"]).addAnswer("Hola, soy un chat bot diseñado para ayudar a los usuarios a resolver sus dudas, estoy en entrenamiento, por favor, se paciente conmigo")
-const flowPrincipal2 = addKeyword(["amor"]).addAnswer("Hola, BB, ¿Cómo estás? ¿En qué puedo ayudarte?")
-const flowPrincipal3 = addKeyword(["hora"]).addAnswer("La hora actual es: " + new Date().toLocaleTimeString())
+const flowBienvenida = addKeyword(EVENTS.WELCOME)
+    .addAnswer('Bienvenido a este chatbot')
+
+const flowAmor = addKeyword(["amor"])
+    .addAnswer("Hola, BB, ¿Cómo estás? ¿En qué puedo ayudarte?")
+
+const flowHora = addKeyword(["hora"])
+    .addAnswer("La hora actual es: " + new Date().toLocaleTimeString())
+
+const flowSaludo = addKeyword('hola')
+    .addAnswer('Hola como estas?', null, async (ctx) => { console.log(ctx) })
+
+const flowDocumento = addKeyword(EVENTS.DOCUMENT)
+    .addAnswer('Documento PDF recibido')
+
+const flowLocation = addKeyword(EVENTS.LOCATION)
+    .addAnswer('Ohh ya veo donde estas')
+
+const flowNotaDeVoz = addKeyword(EVENTS.VOICE_NOTE)
+    .addAnswer('Dame un momento para escuchar la nota de voz')
+
+const flowRecibirMedia = addKeyword(EVENTS.MEDIA)
+    .addAnswer('He recibido tu foto o video')
+
+const flowLlamar = addKeyword('llamar')
+    .addAnswer('Te llamaremos en breve')
+    .addAction(async (ctx) => console.log(`Estan llamando desde: ${ctx.from}`))
+
+const flowEmail = addKeyword('email')
+    .addAction(async (_, { flowDynamic }) => {
+        return await flowDynamic('Por favor, ingresa tu email')
+    })
+    .addAction({ capture: true }, async (ctx, { flowDynamic }) => {
+        const mensaje = ctx.body
+        return await flowDynamic(`Tu email es: ${mensaje}`)
+    })
 
 // #region ConexionOpenAI
-// const flowPrincipal = addKeyword(EVENTS.WELCOME)
-// .addAction(async (ctx, ctxFn) => {
-//     const prompt = "Soy un chat bot diseñado para ayudar a los usuarios a resolver sus dudas";
-//     const text = ctx.body;
+const flowOpenAi = addKeyword("ia")
+    .addAction(async (ctx, ctxFn) => {
+        const prompt = "¿Qué es una IA?"
+        const text = ctx.body
 
-//     const conversations = [];
+        const conversations = []
 
-//     // Crea el contexto con las operaciones 
-//     const contextMessages = conversations.flatMap((conv) => [
-//         { role: "user", content: conv.question },
-//         { role: "assistant", content: conv.answer }
-//     ]);
+        const contextMessages = conversations.flatMap((conversation) => [
+            { role: 'user', content: conversation.question },
+            { role: 'assistant', content: conversation.answer }
+        ])
 
-//     // Añadir la pregunta al contexto
-//     contextMessages.push({ role: "user", content: text });
+        contextMessages.push({ role: 'user', content: text })
 
-//     // Obtener la respuesta de OpenAI
-//     const response = await chat(prompt, contextMessages);
+        const response = await chat(prompt, contextMessages)
 
-//     // Enviar la respuesta al usuario
-//     await ctxFn.flowDynamic(response);
-// });
+        await ctxFn.flowDynamic(response)
+    })
+
 // #endregion
 
 const main = async () => {
     const adapterDB = new MockAdapter()
     const adapterFlow = createFlow([
-        flowPrincipal,
-        flowPrincipal2,
-        flowPrincipal3
+        // flowBienvenida,
+        flowSaludo,
+        flowAmor,
+        flowHora,
+        flowDocumento,
+        flowLocation,
+        flowNotaDeVoz,
+        flowRecibirMedia,
+        flowLlamar,
+        flowEmail,
+        flowOpenAi
     ])
 
     const adapterProvider = createProvider(BaileysProvider)
